1df94057326d478742df6ad0ee1314f6b79abf20
Erlan Aytpaev <aytpaev@gmail.com>
2016-09-28 21:34:35 +0600

    Close #2 issue: use config.json

--- pomodoro.go
+++ pomodoro.go
@@ -1,36 +1,66 @@
 package main
 
 import (
+	"encoding/json"
 	"flag"
 	"fmt"
+	"os"
 	"time"
 )
 
 type Pomodoro struct {
-	work, smallRest, bigRest int
+	Work               Work
+	SmallRest, BigRest Rest
+}
+
+type Work struct {
+	Argument    string
+	Value       int
+	Description string
+}
+
+type Rest struct {
+	Argument             string
+	Value                int
+	Description, Message string
 }
 
 func main() {
+	configFile, err := os.Open("config.json")
+	if err != nil {
+		fmt.Println("File config.json was not found, please check.")
+		os.Exit(1)
+	}
+	defer configFile.Close()
+
+	var pomodoro Pomodoro
+	if err = json.NewDecoder(configFile).Decode(&pomodoro); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
 	// Initialize a new Pomodoro application
-	work := flag.Int("w", 25, "time for working interval (default 25 minutes)")
-	smallRest := flag.Int("sr", 5, "time for small rest between working intervals (default 5 minutes)")
-	bigRest := flag.Int("br", 15, "time for small rest between working intervals (default 15 minutes)")
+	work := flag.Int(pomodoro.Work.Argument, pomodoro.Work.Value, pomodoro.Work.Description)
+	smallRest := flag.Int(pomodoro.SmallRest.Argument, pomodoro.SmallRest.Value, pomodoro.SmallRest.Description)
+	bigRest := flag.Int(pomodoro.BigRest.Argument, pomodoro.BigRest.Value, pomodoro.BigRest.Description)
 	flag.Parse()
-	pomodoro := Pomodoro{work: *work, smallRest: *smallRest, bigRest: *bigRest}
+	pomodoro.Work.Value = *work
+	pomodoro.SmallRest.Value = *smallRest
+	pomodoro.BigRest.Value = *bigRest
 
 	var timer *time.Timer
 	for {
 		for step := 0; step < 3; step++ {
-			timer = time.NewTimer(time.Minute * time.Duration(pomodoro.work))
+			timer = time.NewTimer(time.Minute * time.Duration(pomodoro.Work.Value))
 			<-timer.C
-			fmt.Println("Working interval is over. Please rest.")
-			timer = time.NewTimer(time.Minute * time.Duration(pomodoro.smallRest))
+			fmt.Println(pomodoro.SmallRest.Message)
+			timer = time.NewTimer(time.Minute * time.Duration(pomodoro.SmallRest.Value))
 			<-timer.C
 		}
-		timer = time.NewTimer(time.Minute * time.Duration(pomodoro.work))
+		timer = time.NewTimer(time.Minute * time.Duration(pomodoro.Work.Value))
 		<-timer.C
-		fmt.Println("You work so long. Please stand up and walk.")
-		timer = time.NewTimer(time.Minute * time.Duration(pomodoro.bigRest))
+		fmt.Println(pomodoro.BigRest.Message)
+		timer = time.NewTimer(time.Minute * time.Duration(pomodoro.BigRest.Value))
 		<-timer.C
 	}
 }
